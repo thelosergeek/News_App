869ff9774e75b9e54150fafdd9a5841206e6257d
Inam Khan <inamkh0001@gmail.com>
2020-05-31 12:53:03 +0530

    Added Progress Bar and Swipe Refresh layout

--- app/src/main/java/in/thelosergeek/newsapp/MainActivity.java
+++ app/src/main/java/in/thelosergeek/newsapp/MainActivity.java
@@ -3,6 +3,7 @@ package in.thelosergeek.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView recyclerView;
     final String API_KEY = "ed194c2dff8048f782e606d6fde91790";
     Adapter adapter;
@@ -28,29 +30,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        swipeRefreshLayout = findViewById(R.id.swipe);
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         String country = getCountry();
         retreiveJson(country,API_KEY);
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retreiveJson(country,API_KEY);
+            }
+        });
+
     }
     public  void retreiveJson(String country, String apikey){
 
+        swipeRefreshLayout.setRefreshing(true);
         Call<Headlines> call = ApiClient.getInstance().getApi().getHeadLines(country,apikey);
         call.enqueue(new Callback<Headlines>() {
             @Override
             public void onResponse(Call<Headlines> call, Response<Headlines> response) {
                 if(response.isSuccessful()&& response.body().getArticles()!=null){
+                    swipeRefreshLayout.setRefreshing(false);
                     articles.clear();
                     articles = response.body().getArticles();
                     adapter = new Adapter(MainActivity.this,articles);
                     recyclerView.setAdapter(adapter);
+
                 }
             }
 
             @Override
             public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
                 Toast.makeText(MainActivity.this,t.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
 
             }
